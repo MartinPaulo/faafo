973f848e37e2d88c9b4611060cc116a105925765
Martin Paulo <martin.paulo@gmail.com>
2015-09-17 14:03:18 +1000

    First cut at naive fix for column mapping issue

--- faafo/api/service.py
+++ faafo/api/service.py
@@ -24,6 +24,7 @@ from kombu.pools import producers
 from oslo_config import cfg
 from oslo_log import log
 from PIL import Image
+from sqlalchemy.dialects.mysql import MEDIUMBLOB
 
 from faafo import queues
 from faafo import version
@@ -81,7 +82,10 @@ class Fractal(db.Model):
     xb = db.Column(db.Float, nullable=False)
     ya = db.Column(db.Float, nullable=False)
     yb = db.Column(db.Float, nullable=False)
-    image = db.Column(db.LargeBinary, nullable=True)
+    if CONF.database-url.startswith('sqlite'):
+        image = db.Column(db.LargeBinary, nullable=True)
+    else:
+        image = db.Column(MEDIUMBLOB, nullable=True)
     generated_by = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
